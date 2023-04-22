3793924e8d8c125c9c89a0e62300cc56180995c4
dewabagas <dewabagasql@gmail.com>
2023-04-22 18:39:43 +0700

    adjust multiselect query button and show tables properties

--- lib/presentation/student_dashboard/tabs/tab_student_playground.dart
+++ lib/presentation/student_dashboard/tabs/tab_student_playground.dart
@@ -2,13 +2,11 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:multi_select_flutter/multi_select_flutter.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqlyze/presentation/core/constants/styles.dart';
 import 'package:sqlyze/presentation/core/helpers/database_helper.dart';
 import 'package:sqlyze/presentation/core/styles/app_colors.dart';
 import 'package:sqlyze/presentation/shared/widgets/buttons/button_primary.dart';
-import 'package:sqlyze/presentation/shared/widgets/inputs/input_primary.dart';
 import 'package:sqlyze/presentation/shared/widgets/inputs/input_secondary.dart';
 import 'package:sqlyze/presentation/shared/widgets/pages/page_decoration_top.dart';
 
@@ -21,13 +19,19 @@ class TabStudentPlayground extends StatefulWidget {
 
 class _TabStudentPlaygroundState extends State<TabStudentPlayground> {
   TextEditingController queryController = TextEditingController();
-  Future<List<Map<String, dynamic>>>? _queryResults;
+  Future<List<Map<String, dynamic>>>? queryResults;
+  Future<List<String>>? tables;
   final List<String> queryItems = [
     'SELECT',
     'INSERT',
     'UPDATE',
     'DELETE',
   ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,47 +44,56 @@ class _TabStudentPlaygroundState extends State<TabStudentPlayground> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(height: 20.h),
-              Text('Tap to use on of these Queries !',
-                  style: TextStyles.labelMedium
-                      .copyWith(color: AppColors.charcoal)),
-              SizedBox(height: 10.h),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 10.h),
-                decoration: BoxDecoration(
-                  color: AppColors.lightGrey4,
-                  border: Border.all(color: AppColors.lightGrey4),
-                  borderRadius: BorderRadius.circular(10.r),
-                ),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: List.generate(4, (index) {
-                      return InkWell(
-                        onTap: () {
-                          setState(() {
-                            if (queryController.text.isEmpty) {
-                              queryController.text = queryItems[index];
-                            } else {
-                              queryController.text = queryController.text +
-                                  ' ' +
-                                  queryItems[index];
-                            }
-                          });
-                        },
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 12.w, vertical: 4.h),
-                          decoration: BoxDecoration(
-                            color: AppColors.secondary,
-                            borderRadius: BorderRadius.circular(30.r),
-                          ),
-                          child: Text(queryItems[index],
-                              style: TextStyles.bodyMedium
-                                  .copyWith(color: AppColors.white)),
-                        ),
-                      );
-                    })),
+              ExpansionTile(
+                shape: const RoundedRectangleBorder(),
+                childrenPadding: EdgeInsets.zero,
+                tilePadding: EdgeInsets.zero,
+                backgroundColor: Colors.transparent,
+                collapsedIconColor: AppColors.charcoal,
+                iconColor: AppColors.primary,
+                title: Text('Tap to use on of these Queries !',
+                    style: TextStyles.labelMedium
+                        .copyWith(color: AppColors.charcoal)),
+                children: [
+                  Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 4.w, vertical: 10.h),
+                    decoration: BoxDecoration(
+                      color: AppColors.lightGrey4,
+                      border: Border.all(color: AppColors.lightGrey4),
+                      borderRadius: BorderRadius.circular(10.r),
+                    ),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: List.generate(4, (index) {
+                          return InkWell(
+                            onTap: () {
+                              setState(() {
+                                if (queryController.text.isEmpty) {
+                                  queryController.text = queryItems[index];
+                                } else {
+                                  queryController.text =
+                                      '${queryController.text} ${queryItems[index]}';
+                                }
+                              });
+                            },
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 12.w, vertical: 4.h),
+                              decoration: BoxDecoration(
+                                color: AppColors.secondary,
+                                borderRadius: BorderRadius.circular(30.r),
+                              ),
+                              child: Text(queryItems[index],
+                                  style: TextStyles.bodyMedium
+                                      .copyWith(color: AppColors.white)),
+                            ),
+                          );
+                        })),
+                  ),
+                ],
               ),
-              SizedBox(height: 20.h),
+              SizedBox(height: 10.h),
               Text('Or you can manually type it here !',
                   style: TextStyles.labelMedium
                       .copyWith(color: AppColors.charcoal)),
@@ -106,23 +119,85 @@ class _TabStudentPlaygroundState extends State<TabStudentPlayground> {
               SizedBox(height: 10.h),
               ButtonPrimary(
                 title: 'Execute Query',
-                onPressed: _executeQuery,
+                onPressed: executeQuery,
               ),
+              SizedBox(height: 10.h),
+              InkWell(
+                  onTap: () {
+                    setState(() {
+                      tables = getAllTables();
+                    });
+                  },
+                  child: Text('Show Tables',
+                      style: TextStyles.bodyVerySmall.copyWith(
+                          color: AppColors.primary,
+                          fontWeight: FontWeight.w700))),
+              Expanded(child: buildTableList()),
               Expanded(
-                child: _buildResultsList(),
+                child: buildQueryResult(),
               ),
             ],
           ),
         ));
   }
 
-  Widget _buildResultsList() {
-    if (_queryResults == null) {
+  Widget buildTableList() {
+    return FutureBuilder<List<String>>(
+      future: tables,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            final tables = snapshot.data!;
+            return ListView.builder(
+              itemCount: tables.length,
+              itemBuilder: (context, index) {
+                return ExpansionTile(
+                  title: Text(tables[index]),
+                  children: [
+                    FutureBuilder<List<Map<String, dynamic>>>(
+                      future: getTableProperties(tables[index]),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          if (snapshot.hasError) {
+                            return Text('Error: ${snapshot.error}');
+                          } else {
+                            final properties = snapshot.data!;
+                            return Column(
+                              children: properties.map((property) {
+                                return ListTile(
+                                  title: Text(property['name'].toString()),
+                                  subtitle: Text(
+                                      'Type: ' + property['type'].toString()),
+                                );
+                              }).toList(),
+                            );
+                          }
+                        } else {
+                          return const SizedBox.shrink();
+                        }
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          }
+        } else {
+          return const SizedBox.shrink();
+        }
+      },
+    );
+  }
+
+  Widget buildQueryResult() {
+    if (queryResults == null) {
       return Center(child: Text('No results'));
     }
 
     return FutureBuilder<List<Map<String, dynamic>>>(
-      future: _queryResults,
+      future: queryResults,
       builder: (BuildContext context,
           AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
@@ -144,18 +219,35 @@ class _TabStudentPlaygroundState extends State<TabStudentPlayground> {
     );
   }
 
-  void _executeQuery() async {
+  void executeQuery() async {
     String query = queryController.text;
     try {
       Database db = await DatabaseHelper().database;
       List<Map<String, dynamic>> results = await db.rawQuery(query);
       setState(() {
-        _queryResults = Future.value(results);
+        queryResults = Future.value(results);
       });
     } catch (e) {
       setState(() {
-        _queryResults = Future.error(e.toString());
+        queryResults = Future.error(e.toString());
       });
     }
+  }
+
+  Future<List<String>> getAllTables() async {
+    log('getAllTables');
+    Database db = await DatabaseHelper().database;
+    final List<Map<String, dynamic>> tables =
+        await db.rawQuery("SELECT name FROM sqlite_master WHERE type='table'");
+    final tableNames = tables.map((table) => table['name'] as String).toList();
+    return tableNames;
+  }
+
+  Future<List<Map<String, dynamic>>> getTableProperties(
+      String tableName) async {
+    Database db = await DatabaseHelper().database;
+    final List<Map<String, dynamic>> columns =
+        await db.rawQuery('PRAGMA table_info($tableName)');
+    return columns;
   }
 }
